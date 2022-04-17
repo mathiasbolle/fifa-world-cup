f52b554c4578b9e9a86763ef41be4e3110d73b3b
Mathias Bolle <mathias.bolle@student.hogent.be>
2022-04-17 09:24:01 +0200

    first commit

---
+++ src/main/java/be/hogent/fifa_world_cup/FifaWorldCupApplication.java
@@ -0,0 +1,13 @@
+package be.hogent.fifa_world_cup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaWorldCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaWorldCupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/hogent/fifa_world_cup/FifaWorldCupApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hogent.fifa_world_cup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaWorldCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
