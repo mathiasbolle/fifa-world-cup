e41f981512cf854cf85d865deb805fe96476440f
Mathias Bolle <mathias.bolle@student.hogent.be>
2022-05-10 10:40:34 +0200

    feat: cleanup security

--- src/main/java/be/hogent/fifa_world_cup/SecurityConfig.java
+++ src/main/java/be/hogent/fifa_world_cup/SecurityConfig.java
@@ -1,12 +1,14 @@
 package be.hogent.fifa_world_cup;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -24,8 +26,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
+
         http.authorizeRequests()
-                .antMatchers("/*").
+                        .antMatchers("/*").hasRole("USER");
+        /*
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/fifa/*").hasRole("USER");
+
+         */
+
+        /*
+        http.authorizeRequests()
+                //.antMatchers(HttpMethod.POST, "fifa/**").
+                        .antMatchers("/*").
                 hasRole("USER");
+
+        /*
+        http.authorizeRequests().
+                antMatchers("/**")
+                .hasRole("ADMIN");
+
+         */
+
+
     }
 }
