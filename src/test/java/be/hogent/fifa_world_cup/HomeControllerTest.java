40b84e4072f6e0993977b8b9721d462f89662178
Mathias Bolle <mathias.bolle@student.hogent.be>
2022-05-10 11:37:20 +0200

    feat: add homeControllerTest

---
+++ src/test/java/be/hogent/fifa_world_cup/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package be.hogent.fifa_world_cup;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+public class HomeControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
+
+    @Test
+    public void testRedirectGet() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isFound())
+                .andExpect(view().name("redirect:/fifa"));
+    }
+}
